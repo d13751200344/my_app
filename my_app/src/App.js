150d6a1021b466041bdbb566e78ec053cb427ead
d13751200344 <d13751200344@gmail.com>
2022-08-18 01:09:24 +0800

    classComponentDoing

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,26 +1,24 @@
 import React from "react";
-import "./styles/style.css";
-import Friend from "./Friend";
-//在 props 做好後，引入 friend.js 去使用做好的 props
+
+//event handling
 
 const App = () => {
-  //先寫出等等要使用的資料內容
-  let friends = [
-    { name: "Harry", age: 17, desc: "Chosen one." },
-    { name: "Ron", age: 17, desc: "Red hair." },
-    { name: "Snap", age: 37, desc: "Half-blooded prince." },
-  ];
-  /* 下方 return 內使用上方資料進行 loop，並將 friend.js 中做的 props (如 props.name，即是底下的 name)與 loop 出的結果『value.name』等做綁定 */
+  //button click handler製作(函數)
+  const buttonHandler = (msg) => {
+    alert(msg);
+  };
+
   return (
     <div>
-      <p>My friends are: </p>
-      {friends.map((value) => (
-        <Friend name={value.name} age={value.age} desc={value.desc} />
-      ))}
+      <button
+        onClick={() => {
+          buttonHandler("How are you?");
+        }}
+      >
+        Hello
+      </button>
     </div>
   );
-};
-//之後將做好的 App 導出給 index.js 使用
+}; //上方 onClick 是 button 的事件，綁定了函數 buttonHandler
 
 export default App;
-// 導出App
--- my_app/src/index.js
+++ my_app/src/index.js
@@ -2,12 +2,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import Nav from "./Nav";
 
 // 渲染的第一個參數是要渲染出的element，第二參數則是要顯示在id=root
 ReactDOM.render(
   <React.StrictMode>
-    <Nav />
     <App />
   </React.StrictMode>,
   document.querySelector("#root")
